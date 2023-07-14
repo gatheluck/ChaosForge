f1c70aef8821d4259728324b1492b3a62609caf9
gatheluck <gatheluck+tech@gmail.com>
2023-07-14 23:00:19 +0900

    feat: add script to parse cvpr.

---
+++ src/cvpr/parser.py
@@ -0,0 +1,83 @@
+from typing import Final, List
+
+import requests
+import logging
+from bs4 import BeautifulSoup
+from pydantic import BaseModel, HttpUrl
+
+logger: Final = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+class Paper(BaseModel):
+    """
+
+    Pydantic model which stores single paper infomation.
+
+    """
+
+    title: str
+    author: str
+    abstract: str
+    cvf: HttpUrl
+    pdf: HttpUrl
+
+
+def get_paper_page_urls(year: int) -> List[str]:
+    """
+
+    Return a list of CVF page URL.
+    The list includes all papers accepted by CVPR.
+
+    Args:
+        year (int): A target year.
+
+    """
+
+    cvf_root_url: Final = "https://openaccess.thecvf.com"
+    cvf_all_paper_url: Final = cvf_root_url + f"/CVPR{year}?day=all"
+
+    html: Final = requests.get(cvf_all_paper_url).text
+    bs: Final = BeautifulSoup(html, "html.parser")
+    parsed_tags = bs.select(".ptitle > a")
+    return [cvf_root_url + parsed_tag.get("href") for parsed_tag in parsed_tags]
+
+
+def parse_paper_page(page_url: str, year: int) -> Paper:
+    """
+
+    Parse a paper page and return Paper object.
+
+    """
+
+    html: Final = requests.get(page_url).text
+    bs: Final = BeautifulSoup(html, "html.parser")
+
+    cvf: Final = page_url
+    pdf: Final = (
+        f"https://openaccess.thecvf.com/content/CVPR{year}/papers/"
+        + page_url.removesuffix(".html").split("/")[-1]
+        + (".pdf")
+    )
+
+    try:
+        title: Final = bs.select_one("#papertitle").text.strip()
+        author: Final = bs.select_one("#authors b").text.strip()
+        abstract: Final = bs.select_one("#abstract").text.strip()
+
+        return Paper(
+            title=title,
+            author=author,
+            abstract=abstract,
+            cvf=cvf,  # type: ignore
+            pdf=pdf,  # type: ignore
+        )
+    except AttributeError:
+        logger.warn(f"Failed to parse `{page_url}`.")
+        return Paper(
+            title="",
+            author="",
+            abstract="",
+            cvf=cvf,  # type: ignore
+            pdf=pdf,  # type: ignore
+        )
---
+++ src/cvpr/scripts/parse_cvf_page.py
@@ -0,0 +1,37 @@
+"""
+
+This script generate json file which includes all CVPR 2023 papers info.
+
+"""
+import json
+import pathlib
+import logging
+from typing import Final
+
+from src.cvpr.parser import get_paper_page_urls, parse_paper_page
+
+
+logger: Final = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+def main(
+    year: int,
+    output_dir: pathlib.Path = pathlib.Path("./data/cvpr/"),
+) -> None:
+    urls: Final = get_paper_page_urls(year)
+    logger.info(f"{len(urls)} urls are founds.")
+
+    papers = list()
+    for i, url in enumerate(urls):
+        logger.info(f"[{i+1}/{len(urls)}] parsing `{url}`.")
+        paper = parse_paper_page(url, year)
+        papers.append(paper.model_dump(mode="json"))
+
+    output_path: Final = output_dir / f"papers{year}.json"
+    with output_path.open("w") as f:
+        json.dump(papers, f)
+
+
+if __name__ == "__main__":
+    main(2022)
